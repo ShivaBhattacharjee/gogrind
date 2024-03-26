d817a9ec406c97baa090b478f909bbf1a1fda880
ShivaBhattacharjee <shivaxwarrior@gmail.com>
2024-03-26 23:44:54 +0530

    added build cmd

--- Basics/TimeModule/UsingTime.go
+++ Basics/TimeModule/UsingTime.go
@@ -8,6 +8,11 @@ import (
 func main() {
 	fmt.Println("Time right now is ")
 	currTime := time.Now()
-	// the exact date and time need to be specified for the time formatting
+	// the time formatting is done using the reference date Mon Jan 2 15:04:05 MST 2006
 	fmt.Print(currTime.Format("01-02-2006 Monday 15:04:05"))
+
+	// well how to add custom date and time
+
+	cusDate := time.Date(2020, time.January, 1, 0, 0, 0, 0, time.UTC)
+	fmt.Println("Custom date is \n", cusDate.Format("01-02-2006 Monday 15:04:05"))
 }
